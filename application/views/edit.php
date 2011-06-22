dfb86316e4d4bab28c790fe51e7d23e10cdf69dd
teguh <new_akaguh@yahoo.co.id>
2011-06-22 12:25:49 +0700

    test

--- application/views/edit.php
+++ application/views/edit.php
@@ -5,7 +5,7 @@
 	<title>Ajax</title>
   <script type="text/javascript" src="<?php echo base_url(); ?>/static/js/jquery-1.6.1.min.js"></script>
   <script type="text/javascript" src="<?php echo base_url(); ?>/static/js/posts.js"></script>
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/static/css/default.css"> 
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/static/css/default.css"> aaaa
 </head>
 <body>
   
